e245582b8f582a9497cc688f4cc09e815141fd20
ddelam <daniel.delamare@live.com>
2023-01-26 08:34:03 -0800

    Added Lucene Service

---
+++ Combiner/Lucene/LuceneService.cs
@@ -0,0 +1,147 @@
+﻿using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuceneVersion = Lucene.Net.Util.Version;
+using LuceneDirectory = Lucene.Net.Store.Directory;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using static Lucene.Net.Index.IndexWriter;
+
+namespace Combiner.Lucene
+{
+    public static class LuceneService
+    {
+        const LuceneVersion luceneVersion = LuceneVersion.LUCENE_30;
+        const int MAX_RESULTS = 1000;
+
+        private static List<PropertyInfo> creatureFields = null;
+
+        public static void LoadAll()
+        {
+            var db = new Database();
+            var mod = db.GetAllMods().First();
+            var allCreatures = db.GetAllCreatures(mod);
+
+            //Open the Directory using a Lucene Directory class
+            string indexName = "example_index";
+            string indexPath = Path.Combine(Environment.CurrentDirectory, indexName);
+            const LuceneVersion luceneVersion = LuceneVersion.LUCENE_30;
+            using (LuceneDirectory indexDir = FSDirectory.Open(indexPath))
+            {
+                //Create an analyzer to process the text 
+                Analyzer standardAnalyzer = new StandardAnalyzer(luceneVersion);
+                // create/overwrite index
+                IndexWriter writer = new IndexWriter(indexDir, standardAnalyzer, true, MaxFieldLength.UNLIMITED);
+
+                foreach (var creature in allCreatures)
+                {
+                    LuceneService.WriteToIndex(writer, creature);
+                }
+
+                writer.Commit();
+            }
+        }
+
+        // Reflection is slow, so we cache it
+        private static List<PropertyInfo> GetFields()
+        {
+            if (creatureFields != null)
+            {
+                return creatureFields;
+            }
+
+            var fields = typeof(Creature).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            creatureFields = fields.ToList();
+            return creatureFields;
+        }
+
+        public static Creature MapToCreature(Document doc)
+        {
+            var creature = new Creature();
+
+            foreach(var field in GetFields())
+            {
+                var docField = doc.GetField(field.Name);
+                if (docField != null)
+                {
+                    field.SetValue(creature, CoerceValue(field, docField.StringValue));
+                }
+            }
+
+            return creature;
+        }
+
+        private static object CoerceValue(PropertyInfo field, string stringValue)
+        {
+            // Handle the dictionaries
+            if (field.PropertyType.IsGenericType)
+            {
+                return JsonConvert.DeserializeObject(stringValue, field.PropertyType);
+            }
+            return Convert.ChangeType(stringValue, field.PropertyType);
+        }
+
+        public static void WriteToIndex(IndexWriter writer, Creature creature)
+        {
+            Document doc = new Document();
+            foreach(var field in GetFields())
+            {
+                // Handle the dictionaries
+                if (field.PropertyType.IsGenericType)
+                {
+                    var serialized = JsonConvert.SerializeObject(field.GetValue(creature));
+                    doc.Add(new Field(field.Name, serialized, Field.Store.YES, Field.Index.ANALYZED));
+                }
+                else if (field.PropertyType == typeof(bool))
+                {
+                    doc.Add(new Field(field.Name, ((bool)field.GetValue(creature) ? "true" : "false"), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS));
+                }
+                else if (field.PropertyType.IsValueType)
+                {
+                    var numField = new NumericField(field.Name, Field.Store.YES, true);
+                    if (field.PropertyType == typeof(int))
+                    {
+                        numField.SetIntValue((int)field.GetValue(creature));
+                    }
+                    else
+                    {
+                        numField.SetDoubleValue((double)field.GetValue(creature));
+                    }
+                    doc.Add(numField);
+                }
+                else
+                {
+                    doc.Add(new Field(field.Name, field.GetValue(creature).ToString(), Field.Store.YES, Field.Index.ANALYZED));
+                }
+            }
+            writer.AddDocument(doc);
+        }
+
+        public static List<Creature> Query(Query q)
+        {
+            string indexName = "example_index";
+            string indexPath = Path.Combine(Environment.CurrentDirectory, indexName);
+            using (LuceneDirectory indexDir = FSDirectory.Open(indexPath))
+            {
+                IndexSearcher searcher = new IndexSearcher(indexDir);
+                TopDocs topDocs = searcher.Search(q, n: MAX_RESULTS);
+                var docs = new List<Creature>();
+                foreach (var doc in topDocs.ScoreDocs)
+                {
+                    docs.Add(MapToCreature(searcher.Doc(doc.Doc)));
+                }
+                return docs;
+            }
+        }
+
+    }
+}
---
+++ CombinerTests/Tests/LuceneTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Combiner.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using System.Diagnostics;
+using LuceneDirectory = Lucene.Net.Store.Directory;
+using System.IO;
+using LuceneVersion = Lucene.Net.Util.Version;
+using static Lucene.Net.Index.IndexWriter;
+using Combiner.Lucene;
+
+namespace Combiner.Tests.Tests
+{
+    [TestClass()]
+    public class LuceneTests
+    {
+        [TestMethod()]
+        public void LoadAll()
+        {
+            LuceneService.LoadAll();
+        }
+
+        [TestMethod]
+        public void TryIndex()
+        {
+            string indexName = "example_index";
+            string indexPath = Path.Combine(Environment.CurrentDirectory, indexName);
+
+            using (LuceneDirectory indexDir = FSDirectory.Open(indexPath))
+            {
+                IndexSearcher searcher = new IndexSearcher(indexDir);
+                Query query = new TermQuery(new Term("Rank", "3"));
+                TopDocs topDocs = searcher.Search(query, n: 1000);
+                var docs = new List<Document>();
+                foreach (var doc in topDocs.ScoreDocs)
+                {
+                    docs.Add(searcher.Doc(doc.Doc));
+                }
+            }
+        }
+
+
+
+        [TestMethod]
+        public void TryLuceneService()
+        {
+            Query query = NumericRangeQuery.NewIntRange("Rank", 3, 3, true, true);
+
+            var vals = LuceneService.Query(query);
+        }
+
+        [TestMethod]
+        public void TestMultiTermQuery()
+        {
+            var bq = new BooleanQuery();
+            var bq2 = new BooleanQuery();
+            Query query = NumericRangeQuery.NewIntRange("Rank", 3, 3, true, true);
+            Query query2 = NumericRangeQuery.NewDoubleRange("EffectiveHitpoints", 200, 230, true, true);
+
+            bq.Add(query, Occur.MUST);
+            bq.Add(query2, Occur.MUST);
+
+            bq2.Add(query, Occur.MUST);
+
+            var v1 = LuceneService.Query(bq);
+            var v2 = LuceneService.Query(query);
+            var v3 = LuceneService.Query(bq2);
+            var vals = LuceneService.Query(query2);
+        }
+    }
+}
